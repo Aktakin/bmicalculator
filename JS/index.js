58bb987bf988391f66fe19fa77dc1ba752d7afe7
Akt Akinro <aakinroo@gmail.com>
2023-03-28 06:46:19 +0100

    first commit

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+var akt = document.getElementById('hello')
+var hi = document.getElementsByClassName('hi')[0]
+var jk = document.getElementsByTagName('p')[0]
+
+jk.innerHTML = 'who be this'
+jk.style.cssText = 'color:red'
+hi.innerHTML = 'goodbye'
+hi.style.cssText = 'color:blue'
+akt.style.cssText = 'color:red; font-size: 60px; text-transform: uppercase'
